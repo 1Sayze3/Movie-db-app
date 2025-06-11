9e80923b2f3a3ce5c1817d4ec9f00f03047ed07e
Sayan Roy <www.sayanroy2017@gmail.com>
2025-06-11 16:18:34 +0530

    Deploy ready

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/movie-db-app/',
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/movie-db-app/' : '/',
+    plugins: [react()],
+  };
+});
